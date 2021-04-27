5a32543cbbd00620f3a6e4adfef27f196ead3e88
Celina Louissaint <celinalouissaint@gmail.com>
2021-04-26 18:29:55 -0700

    added query to return data from candidates table

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-// Express middleware
+// --------------------------- Express middleware  --------------------------- //
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// --------------------------- Connect to Database  --------------------------- // 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username,
+        user: 'root',
+        // your MySQL password
+        password: 'Y0usuck@7',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// return all data in candidates table 
+db.query('Select * FROM candidates', (err, rows) => {
+    console.log(rows);
 });
 
-
-
-// Default response for any other request (Not Found)
+// Default response for any other request (Not Found) *catchall route* must be last
 app.use((req, res) => {
     res.status(404).end();
 });
