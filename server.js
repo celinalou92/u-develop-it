54d0d8bbc333bb687e9cc28bb08808e1d5b77133
Celina Louissaint <celinalouissaint@gmail.com>
2021-04-28 18:41:14 -0700

    add CRUD routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const inputCheck = require('./utils/inputCheck');
 // --------------------------- Express middleware  --------------------------- //
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,12 +22,29 @@ const db = mysql.createConnection(
     console.log('Connected to the election database')
 )
 
+// get all candidates before express route
 // return all data in candidates table 
 // db.query('Select * FROM candidates', (err, rows) => {
 //     console.log(rows);
 // });
 
-// GET a single candidate
+// Get all candidates after express route
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// GET a single candidate before express route
 // db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 //     if (err) {
 //       console.log(err);
@@ -33,7 +52,26 @@ const db = mysql.createConnection(
 //     console.log(row);
 // });
 
-// DELETE a candidate
+// Get a single candidate after express route
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+
+// DELETE a candidate before express route
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) =>{
 //     if (err) {
 //         console.log(err);
@@ -41,7 +79,30 @@ const db = mysql.createConnection(
 //     console.log(result)
 // })
 
-// CREATE a candidate
+// DELETE a candidate after express route
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+
+// CREATE a candidate before express route
 // const sql = `INSERT INTO candidates(id, first_name, last_name, industry_connected)
 //     VALUES(?,?,?,?)`;
 // const params = [1, 'Ronald', 'Firbank', 1];
@@ -54,21 +115,38 @@ const db = mysql.createConnection(
 // })
 
 
-// ================================ pick back up at 12.2.5  ================================ //
+// CREATE a candidate after express route
+app.post('/api/candidate', ({ body }, res) => {
+    // inputCheck function provided by class work
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    // db query block 
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+     VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+    res.json({
+        message: 'success',
+        data: body
+    });
+    });
+  });
+
+// ================================ pick back up at 12.3.5  ================================ //
 
 
 // Default response for any other request (Not Found) *catchall route* must be last
 app.use((req, res) => {
     res.status(404).end();
 });
-
-
-
-
-
-
-
-
 
 
 app.listen(PORT, () => {
